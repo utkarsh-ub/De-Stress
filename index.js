958874027bbf4ba477ff0fac106e5b08f0430d3d
Utkarsh Bhaskar <utkarshbhaskar@05@gmail.com>
2021-06-27 14:30:33 +0530

    Linked the products

---
+++ index.js
@@ -0,0 +1,19 @@
+function nirva()
+{
+    window.open("melody.html");
+}
+
+function hands()
+{
+    window.open("asmr.html");
+}
+
+function talks()
+{
+    window.open("chat/uttu.html");
+}
+
+function news()
+{
+    window.open("news/index.html");
+}
